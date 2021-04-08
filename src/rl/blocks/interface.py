5d8b265d2486fd121f756e1ef3dc33cc285d6b67
Josh Fourie <josh@aequita.io>
2021-04-08 12:45:39 +0000

    [v0.1] move Display ABC into rl for backwards compatability.
    Signed-off-by: Josh Fourie <josh@aequita.io>

--- src/rl/blocks/interface.py
+++ src/rl/blocks/interface.py
@@ -2,13 +2,10 @@
 The .blocks submodule is responsible for combining the building blocks of reinforcement learning agents in .engine with visualisations in .widgets
 '''
 
-from abc import abstractmethod
+from abc import abstractmethod, ABC
 
 from result.result import Err, Result
 from typing import Any, Generic, TypeVar
-
-from ...showcase.interface import Display
-
 
 T = TypeVar('T')
 
@@ -22,6 +19,35 @@ B = TypeVar('B')
 C = TypeVar('C')
 D = TypeVar('D')
 E = TypeVar('E')
+
+T = TypeVar('T')
+U = TypeVar('U')
+V = TypeVar('V')
+
+
+class Display(Generic[T, U, V], ABC):
+    '''
+    ABC for things that can return a display to a showcase runner.
+
+    Generics:
+        T : something that can be rendered by a showcase
+        U : some parent to callback to
+        V : some children to callback to
+    '''
+
+    @abstractmethod
+    def display(self, parent: U, children: V, **kwargs: Any) -> T:
+        '''
+        Displays the block in a showcase environment.
+        '''
+        raise NotImplementedError
+
+    @abstractmethod
+    def update(self, parent: U, children: V, **kwargs: Any) -> T:
+        '''
+        Updates a block in a showcase environment.
+        '''
+        raise NotImplementedError
 
 class MemoryBlock(Generic[T, B, C, E], Display[Any, B, C]):
     '''
--- src/showcase/interface.py
+++ src/showcase/interface.py
@@ -4,36 +4,3 @@ The .showcase submodule is responsible for managing the displaying of the assemb
 This file is an interface for upstream modules to interact with the .showcase module.
 '''
 
-# built-in libraries
-from typing import Any, Generic, TypeVar
-from abc import ABC, abstractmethod
-
-
-T = TypeVar('T')
-U = TypeVar('U')
-V = TypeVar('V')
-
-
-class Display(Generic[T, U, V], ABC):
-    '''
-    ABC for things that can return a display to a showcase runner.
-
-    Generics:
-        T : something that can be rendered by a showcase
-        U : some parent to callback to
-        V : some children to callback to
-    '''
-
-    @abstractmethod
-    def display(self, parent: U, children: V, **kwargs: Any) -> T:
-        '''
-        Displays the block in a showcase environment.
-        '''
-        raise NotImplementedError
-
-    @abstractmethod
-    def update(self, parent: U, children: V, **kwargs: Any) -> T:
-        '''
-        Updates a block in a showcase environment.
-        '''
-        raise NotImplementedError
