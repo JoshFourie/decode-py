29cd87ff91b16e729c9c8ceac1695ef89dc8cd67
Josh Fourie <josh@aequita.io>
2021-04-09 04:18:15 +0000

    [v0.1] test: add from databse for simple display template factory.
    Signed-off-by: Josh Fourie <josh@aequita.io>

--- src/showcase/register/tests/test_simple.py
+++ src/showcase/register/tests/test_simple.py
@@ -50,12 +50,12 @@ def test_simple_display_database_userdict_semantics() -> None:
 
 
 '''
-Unit tests for SimpleDisplayTemplateFactory.
+Unit tests for `SimpleDisplayTemplateFactory`.
 '''
 
 def test_simple_display_template_factory() -> None:
     '''
-    Tests that SimpleDisplayTemplateFactory can get a template.
+    Tests that `SimpleDisplayTemplateFactory` can get a template.
     '''
     # test setup
     node_key: nodeKey = 'node_key'
@@ -77,7 +77,41 @@ def test_simple_display_template_factory() -> None:
     assert database.make_template(node_key) == displayable_template
 
 
-# def test_simple_display_template_factory_from_simple_display_database() -> None:
-#     '''
-#     Tests that 
-#     '''
+def test_simple_display_template_factory_from_simple_display_database() -> None:
+    '''
+    Tests that a `SimpleDisplayTemplateFactory` can be instantiated from a `SimpleDisplayableDatabase`
+    '''
+    # test setup
+    node_key: nodeKey = 'node_key'
+
+    displayable_template: displayableTemplate = 'displayable_template'
+
+    alt_displayable_template: displayableTemplate = 'alt_displayable_template'
+
+    ## get a source database
+
+    source_database = SimpleDisplayablesDatabase[displayableTemplate]()
+
+    source_database.update({ node_key : displayable_template })
+
+    # test instance 
+
+    test_instance = SimpleDisplayableTemplateFactory[displayableTemplate]()
+    
+    test_instance.from_database(database = source_database)
+
+    ## does a test instance copy the data?
+
+    test = test_instance.lookup_node(node_key = node_key)
+
+    assert test == displayable_template, 'expected to copy data from source database'
+
+    ## does a test instance copy a reference to the source data?
+
+    source_database.update({ node_key : alt_displayable_template })
+
+    test = test_instance.lookup_node(node_key = node_key)
+
+    assert test == alt_displayable_template, 'expected to copy a reference to data from source database'
+
+    return None
