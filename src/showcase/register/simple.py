761c227f5c63651b2404db1f1c5aa7e525e0c737
Josh Fourie <josh@aequita.io>
2021-04-09 03:18:45 +0000

    [v0.1] fix: move simple mediator to bottom of file.
    Signed-off-by: Josh Fourie <josh@aequita.io>

--- src/showcase/register/simple.py
+++ src/showcase/register/simple.py
@@ -23,7 +23,78 @@ SimpleDisplayableTemplate: TypeAlias = NotImplementedError
 
 SimpleDisplayableSchema: TypeAlias = SimpleDisplayableTemplate
 
-SimpleDisplayableBuilder: TypeAlias = DisplayableBuilder[HashableNodeKey, SimpleMemento, SimpleDisplayable]
+class SimpleDisplayablesDatabase\
+(
+    Generic[DisplayableSchema], 
+    DisplayablesDatabase[HashableNodeKey, DisplayableSchema],
+    UserDict # type checks sometimes think this needs type arguments but that is a syntax error
+):
+    '''
+    Class that can look up a `SimpleDisplayableSchema` with a `HashableNodeKey` stored in a dict-like structure.
+    '''
+
+    def lookup_node(self, node_key: HashableNodeKey, *args: Any, **kwargs: Any) -> DisplayableSchema:
+        '''
+        Looks up the node key in a dict-like structure.
+
+        Throws a `KeyError` if the key node key is not in the dict.
+        '''
+        lookup: Union[DisplayableSchema, None] = self.get(key = node_key, default = None)
+
+        if lookup is None: raise KeyError('looking up %s returned a None' % (node_key, lookup))
+
+        return lookup
+
+
+class SimpleDisplayableTemplateFactory\
+(
+    Generic[DisplayableTemplate],
+    DisplayableTemplateFactory[HashableNodeKey, DisplayableTemplate],
+    SimpleDisplayablesDatabase[DisplayableTemplate]
+):
+    '''
+    Class that can make a `DisplayableTemplate` using 
+    a stored `Dict` of `NodeKeys` that map to a `DisplayableSchema`.
+    '''
+    
+    def make_template(self, node_key: HashableNodeKey, *args: Any, **kwargs: Any) -> DisplayableTemplate:
+        '''
+        Makes a `DisplayableTemplate` by adapting a `DisplayableSchema` associated with 
+        the given `NodeKey`.
+        '''
+        return self.lookup_node(node_key)
+
+    def from_database(self, database: SimpleDisplayablesDatabase[DisplayableTemplate]) -> None:
+        '''
+        Stores a reference to the data in a `SimpleDisplayablesDatabase` to this instance.
+        '''
+        data: Dict[HashableNodeKey, DisplayableTemplate] = database.data
+
+        self.data = data
+
+        return None
+
+
+class SimpleDisplayableBuilder\
+(
+    DisplayableBuilder[HashableNodeKey, SimpleMemento, SimpleDisplayable]
+):
+    '''
+    Class that can build a `Displayable` from a `HashableNodeKey` and `SimpleMemento`
+    '''
+    
+    '''
+    ABC extensions.
+    '''
+
+    def build_displayable(self, node_key: HashableNodeKey, node_memento: SimpleMemento, *args: Any, **kwargs: Any) -> SimpleDisplayable:
+        '''
+        Builds a `Displayable` from a `HashableNodeKey` and `SimpleMemento`
+        '''
+        # display_template: SimpleDisplayableTemplate 
+        raise NotImplementedError('require property methods to get the underlying components.')
+
+
 
 
 class SimpleRegisterMediator\
@@ -97,55 +168,3 @@ class SimpleRegisterMediator\
         raise NotImplementedError('still require a concrete `SimpleDisplayBuilder` class to map a `SimpleNodeMemento` to a `Displayable`')
 
         return displayable_builder
-
-
-class SimpleDisplayablesDatabase\
-(
-    Generic[DisplayableSchema], 
-    DisplayablesDatabase[HashableNodeKey, DisplayableSchema],
-    UserDict # type checks sometimes think this needs type arguments but that is a syntax error
-):
-    '''
-    Class that can look up a `SimpleDisplayableSchema` with a `HashableNodeKey` stored in a dict-like structure.
-    '''
-
-    def lookup_node(self, node_key: HashableNodeKey, *args: Any, **kwargs: Any) -> DisplayableSchema:
-        '''
-        Looks up the node key in a dict-like structure.
-
-        Throws a `KeyError` if the key node key is not in the dict.
-        '''
-        lookup: Union[DisplayableSchema, None] = self.get(key = node_key, default = None)
-
-        if lookup is None: raise KeyError('looking up %s returned a None' % (node_key, lookup))
-
-        return lookup
-
-
-class SimpleDisplayableTemplateFactory\
-(
-    Generic[DisplayableTemplate],
-    DisplayableTemplateFactory[HashableNodeKey, DisplayableTemplate],
-    SimpleDisplayablesDatabase[DisplayableTemplate]
-):
-    '''
-    Class that can make a `DisplayableTemplate` using 
-    a stored `Dict` of `NodeKeys` that map to a `DisplayableSchema`.
-    '''
-    
-    def make_template(self, node_key: HashableNodeKey, *args: Any, **kwargs: Any) -> DisplayableTemplate:
-        '''
-        Makes a `DisplayableTemplate` by adapting a `DisplayableSchema` associated with 
-        the given `NodeKey`.
-        '''
-        return self.lookup_node(node_key)
-
-    def from_database(self, database: SimpleDisplayablesDatabase[DisplayableTemplate]) -> None:
-        '''
-        Stores a reference to the data in a `SimpleDisplayablesDatabase` to this instance.
-        '''
-        data: Dict[HashableNodeKey, DisplayableTemplate] = database.data
-
-        self.data = data
-
-        return None
