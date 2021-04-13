6aee83bb613735a26c810322f5295b501b09f2f8
Josh Fourie <josh@aequita.io>
2021-04-13 00:59:41 +0000

    [v0.1] chore: add doc comment.
    Signed-off-by: Josh Fourie <josh@aequita.io>

--- src/showcase/register/types.py
+++ src/showcase/register/types.py
@@ -2,6 +2,8 @@
 Generic types for this module.
 
 Semi-stable API for child modules.
+
+These are generally things that the caller needs to implement, or have access to an implementation.
 '''
 
 from typing import TypeVar
