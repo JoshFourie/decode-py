e27313d1802f6953325b1e769baa7f63cb97ee9d
Josh Fourie <josh@aequita.io>
2021-05-25 05:39:33 +0000

    [v0.1] example: add imports for example.
    Signed-off-by: Josh Fourie <josh@aequita.io>

--- src/example/MCR2/components.py
+++ src/example/MCR2/components.py
@@ -1,3 +1,15 @@
 '''
 Components for the MCR2 example.
 '''
+
+import sys; sys.path.append('../')  # silly hack to get the zalia package
+
+# library imports
+from src.showcase.maker.simple import SimpleMakerFacade
+from src.showcase.assembler.simple import SimpleAssembler
+from src.showcase.database.simple import SimpleGraphDB
+
+# external imports
+from torch import Tensor
+
+
