7cb2dc337eec48abf8922d17edb1636bb5c55aaf
Josh Fourie <josh@aequita.io>
2021-05-25 06:44:53 +0000

    [v0.1] example: add mixed coding rate strategy blocks.
    Signed-off-by: Josh Fourie <josh@aequita.io>

--- src/example/MCR2/components.py
+++ src/example/MCR2/components.py
@@ -7,13 +7,8 @@ import sys; sys.path.append('../')  # silly hack to get the zalia package
 # built-in imports
 from typing_extensions import TypeAlias
 
-# library imports
-from src.showcase.maker.simple import SimpleMakerFacade
-from src.showcase.assembler.simple import SimpleAssembler
-from src.showcase.database.simple import SimpleGraphDB
-
 # external imports
-from torch import Tensor, eye
+from torch import Tensor, eye, zeros
 
 
 '''
@@ -24,6 +19,16 @@ ExpansionMatrixType: TypeAlias = Tensor
 
 CompressionMatrixType: TypeAlias = Tensor
 
+ClassMembershipType: TypeAlias = Tensor
+
+StackedClassMembershipType: TypeAlias = Tensor
+
+FeatureMatrixType: TypeAlias = Tensor
+
+EpsilonHyperParameterType: TypeAlias = float
+
+MaximumCodingRateType: TypeAlias = float
+
 
 class ExpansionMatrixFactory:
     '''
@@ -31,7 +36,7 @@ class ExpansionMatrixFactory:
     '''
 
     @classmethod
-    def make_expansion_matrix(cls, input: Tensor, epsilon: int) -> ExpansionMatrixType:
+    def make_expansion_matrix(cls, input: FeatureMatrixType, epsilon: EpsilonHyperParameterType) -> ExpansionMatrixType:
         '''
         Makes an `ExpansionMatrixType` from `input` and `epsilon`.
         '''
@@ -50,7 +55,7 @@ class CompressionMatrixFactory:
     '''
 
     @classmethod
-    def make_compression_matrix(cls, input: Tensor, memberships: Tensor, epsilon: int) -> CompressionMatrixType:
+    def make_compression_matrix(cls, input: FeatureMatrixType, membership: ClassMembershipType, epsilon: EpsilonHyperParameterType) -> CompressionMatrixType:
         '''
         Makes a `CompressionMatrixType` from `input`, `memberships` and `epsilon`.
         '''
@@ -59,4 +64,52 @@ class CompressionMatrixFactory:
         I: Tensor = eye(d)
         alpha: float = d / (m*epsilon)
 
-        return alpha * (I + alpha * input@memberships@input.T).inverse()
+        return alpha * (I + alpha * input@membership@input.T).inverse()
+
+
+class CompressionStrategy:
+    '''
+    Strategy that can use a `StackedClassMembershipType` to compress the coding rate of a `FeatureMatrixType` instance.
+    '''
+
+    @classmethod
+    def apply_compression_strategy(cls, input: FeatureMatrixType, memberships: StackedClassMembershipType, epsilon: EpsilonHyperParameterType) -> FeatureMatrixType:
+        '''
+        Applies a compression strategy to compute the coding rate of the `input` using a `CompressionMatrixType` for each class in this `memberships` instance.
+        ''' 
+        d, m = input.shape
+  
+        compression: Tensor = zeros(d, m)
+        
+        membership: ClassMembershipType  # type declaration
+
+        for membership in memberships:
+
+            compression_matrix = CompressionMatrixFactory.make_compression_matrix(input = input, membership = membership, epsilon = epsilon)
+            
+            trace: Tensor = membership.trace() + 1e-8
+            gamma: Tensor = trace / m
+
+            compression += gamma * compression_matrix@input@membership
+        
+        return compression
+
+
+class MaximumCodingRateStrategy:
+    '''
+    Class that can transform a `FeatureMatrixType` to increase the difference between the mixed and simple coding rates of the distribution.
+    '''
+
+    @classmethod
+    def apply_coding_rate_strategy(cls, input: FeatureMatrixType, memberships: ClassMembershipType, epsilon: EpsilonHyperParameterType) -> FeatureMatrixType:
+        '''
+        Applies a coding rate strategy to transform the `input` into a distribution with a better coding rate.
+        '''
+        expansion_matrix = ExpansionMatrixFactory.make_expansion_matrix(input = input, epsilon = epsilon)
+
+        expanded_features = expansion_matrix@input
+
+        compressed_features = CompressionStrategy.apply_compression_strategy(input = input, memberships = memberships, epsilon = epsilon)
+
+        return input + (expanded_features - compressed_features) 
+
